0275a78647efe77e7c2d58a99b9e2abde56c327c
Omar Hamed <hamedomarkhaled@gmail.com>
2020-10-04 09:03:13 +0200

    Upgrade to Junit5.Closes #4

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/SfgPetClinicApplication.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/SfgPetClinicApplication.java
@@ -2,8 +2,6 @@ package guru.springframework.sfgpetclinic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class SfgPetClinicApplication {
